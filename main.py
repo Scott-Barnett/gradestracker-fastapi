38003686ebb9bbd6c70e31008f5c21a86a58ce17
Scott Barnett <sacbarnett@gmail.com>
2021-04-22 00:01:25 +0100

    Add initial FastAPI project

---
+++ main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+from fastapi import FastAPI
+import uvicorn
+import json
+
+with open("local_config.json") as json_file:
+    local_config = json.load(json_file)
+
+app = FastAPI()
+
+import api.routes
+
+if __name__ == "__main__":
+    if local_config["DEBUG"]:
+        print("Starting server in DEBUG mode - Edit local_config.json to change this")
+    uvicorn.run("main:app", reload=local_config["DEBUG"])
