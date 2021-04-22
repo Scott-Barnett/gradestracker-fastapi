bb5903dea3aa2afa7896daee21505754113c9b04
Scott Barnett <sacbarnett@gmail.com>
2021-04-22 02:06:54 +0100

    Finish up auth checks
    To protect an endpoint, simply depend on a user

--- api/routes.py
+++ api/routes.py
@@ -2,7 +2,7 @@ from fastapi import Depends, Request, Response, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
 from typing import Optional
 
-from main import app
+from main import app, oauth_scheme
 from .models import LocalSession, User
 from .schema import Token
 from .authentication import PasswordManager, TokenManager
@@ -29,9 +29,24 @@ def get_user(db, username: str, password: str) -> Optional[User]:
         return possible_user
 
 
+def require_login(db=Depends(get_db), token: str = Depends(oauth_scheme)) -> User:
+    exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"}
+    )
+    username = TokenManager.validate_token(token)
+    if username is None:
+        raise exception
+    user = db.query(User).filter(User.username == username).first()
+    if user is None:
+        raise exception
+    return user
+
+
 @app.get("/")
-def index(db=Depends(get_db)):
-    return {"message": "hello, world"}
+def index(user: User = Depends(require_login)):
+    return {"message": f"hello, {user.username}"}
 
 
 @app.post("/token", response_model=Token)
