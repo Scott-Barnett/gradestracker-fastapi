34c96918c8b21a040ccbee1f8b4a6d0d008bfc60
Scott Barnett <sacbarnett@gmail.com>
2021-04-22 01:24:24 +0100

    Add Password Hashing (for authentication)

--- api/authentication.py
+++ api/authentication.py
@@ -1,5 +1,6 @@
 from jose import jwt, JWTError
 from datetime import datetime, timedelta
+from passlib.context import CryptContext
 from typing import Optional
 
 from main import local_config
@@ -28,3 +29,15 @@ class TokenManager:
             return decoded["sub"]
         except JWTError:
             return None
+
+
+class PasswordManager:
+    PW_CONTEXT = CryptContext(schemes=[local_config["PASSWORD_MANAGER"]["HASH_SCHEME"]], deprecated=["auto"])
+
+    @staticmethod
+    def hash_password(password: str) -> str:
+        return PasswordManager.PW_CONTEXT.hash(password)
+
+    @staticmethod
+    def verify_password(password: str, password_hash: str) -> bool:
+        return PasswordManager.PW_CONTEXT.verify(password, password_hash)
